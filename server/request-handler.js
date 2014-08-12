004628646d475ee12fdefc9938650b3c20d5df7d
nrobeR <mr.nrober@gmail.com>
2014-08-11 19:49:53 -0700

    Added basic message storage and request handling.

--- server/request-handler.js
+++ server/request-handler.js
@@ -4,6 +4,9 @@
  * You'll have to figure out a way to export this function from
  * this file and include it in basic-server.js so that it actually works.
  * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
+  var serverData = {
+    'results':[]
+  };
 
 var handleRequest = function(request, response) {
   /* the 'request' argument comes from nodes http module. It includes info about the
@@ -12,9 +15,35 @@ var handleRequest = function(request, response) {
   /* Documentation for both request and response can be found at
    * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
 
+
+   console.log("REQUEST URL IS: "+request.url);
+   var statusCode = 404;
+
+   if(request.method === 'GET'){
+    if(request.url === '/classes/messages'){
+      statusCode = 200;
+    }
+    if(request.url === '/classes/room'){
+      statusCode = 200;
+    }
+   }
+
+   if(request.method === 'POST'){
+    request.on('data',function(data){
+      var parsedData = JSON.parse(data);
+      var newMessage = {};
+      newMessage.username = parsedData['username'];
+      newMessage.message = parsedData['message'];
+      serverData.results.push(newMessage);
+    });
+    //if(request.url === 'classes/messages'){
+      statusCode = 201;
+    //}
+   }
+
   console.log("Serving request type " + request.method + " for url " + request.url);
 
-  var statusCode = 200;
+
 
   /* Without this line, this server wouldn't work. See the note
    * below about CORS. */
@@ -23,15 +52,17 @@ var handleRequest = function(request, response) {
   headers['Content-Type'] = "text/plain";
 
   /* .writeHead() tells our server what HTTP status code to send back */
-  response.writeHead(statusCode, headers);
+  // response.writeHead(statusCode, headers);
+  response.statusCode = statusCode;
 
   /* Make sure to always call response.end() - Node will not send
    * anything back to the client until you do. The string you pass to
    * response.end() will be the body of the response - i.e. what shows
    * up in the browser.*/
-  response.end("Hello, World!");
-};
 
+  response.end(JSON.stringify(serverData));
+};
+module.exports = handleRequest;
 /* These headers will allow Cross-Origin Resource Sharing (CORS).
  * This CRUCIAL code allows this server to talk to websites that
  * are on different domains. (Your chat client is running from a url
